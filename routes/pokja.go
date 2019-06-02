6f7e0c474d045d93a3b2b047f18c6f6f3db2e3a5
Eore <eore26@gmail.com>
2019-06-02 19:17:27 +0700

    penambahan route method

--- routes/pokja.go
+++ routes/pokja.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/eore/sismadak/handlers"
@@ -9,14 +10,18 @@ import (
 
 func RouterPokja(router *http.ServeMux, handler handlers.Pokja) {
 	router.HandleFunc("/pokja", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		log.Println(r.Method, r.RemoteAddr)
 		switch r.Method {
 		case "GET":
 			handler.ListPokja(w, r)
 		case "POST":
 			handler.TambahPokja(w, r)
+		case "PUT":
+			handler.EditPokja(w, r)
 		default:
-			fmt.Fprintln(w, "method salah")
+			w.WriteHeader(404)
+			fmt.Fprintln(w, `"method salah"`)
 		}
 	})
-	router.HandleFunc("/pokja/", handler.ListPokja)
 }
